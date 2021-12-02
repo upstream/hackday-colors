a6fdf35308b71d489f5a39501e16643e14751140
carmem <carmem@cobot.me>
2021-12-02 12:46:41 +0100

    first commit

---
+++ src/_data/colors.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    name: "primary",
+    color: "hsl(210, 79%, 35%)",
+    onColor: "hsl(0, 0%, 100%)",
+  },
+  {
+    name: "error",
+    color: "hsl(348, 99%, 45%)",
+  },
+  {
+    name: "text",
+    color: "hsl(229, 31%, 14%)",
+  },
+];
